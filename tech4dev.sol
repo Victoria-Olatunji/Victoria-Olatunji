f3705ac1503b1fb4e862abd5a966f0f046d3ec0c
Victoria Olatunji <aboladegeovhykee@gmail.com>
2022-09-26 11:45:57 +0100

    first commit

---
+++ tech4dev.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract tech4dev{
+
+    //array struct
+
+    struct Person{
+        string name;
+        uint age;
+    }
+    
+    Person("victoria",13);
+}
